eaf1c556f6cf14904b88ef75df87ce5bbb2b750c
pooria alimardani <p.javawork@gmail.com>
2020-04-11 21:30:26 +0430

    - Changes footer.php page

--- mvc/view/page/footer.php
+++ mvc/view/page/footer.php
@@ -1,67 +1,65 @@
-<div class="footer-box">
-
-    <!-- Footer img-->
-    <div class="row-footer">
-        <img src="/MainProject/image/CashOnSpot.png" class="img-footer" id="img-cash" alt="Cash">
-        <img src="/MainProject/image/Express.png" class="img-footer" id="img-express" alt="Express">
-        <img src="/MainProject/image/Return.png" class="img-footer" id="img-return" alt="Return">
-        <img src="/MainProject/image/Support.png" class="img-footer" id="img-support" alt="Support">
-        <img src="/MainProject/image/Warranty.png" class="img-footer" id="img-warranty" alt="Warranty">
-    </div>
-
-    <!-- Footer txt-->
-    <div>
-        <i class="txt-cash">پرداخت در محل</i>
-        <i class="txt-express">تحویل اکسپرس</i>
-        <i class="txt-return">7 روز زمان بازگشت</i>
-        <i class="txt-support">پشتیبانی 24 ساعته</i>
-        <i class="txt-warranty">ضمانت اصل بودن کالا</i>
-    </div>
-    <br>
-    <hr class="horizontal-line4">
-
-    <!-- Footer Link -->
-    <div class="footer-link">
+<div id="box-footer">
+    <div class="container">
         <div class="row">
-            <br>
-            <div class="col-sm-6">
-                <i class="footer-header-net">در شبکه های اجتماعی دنبال کنید</i>
-                <br><br>
-
-                <!-- Footer Apps -->
-                <div class="col-sm-2">
-                    <span style="font-size: x-large;">
-                        <i class="fa fa-youtube-square footer-icon-brand"></i>
-                    </span>
-                </div>
-                <div class="col-sm-2">
-                    <span style="font-size: x-large">
-                        <i class="fa fa-instagram footer-icon-brand"></i>
-                    </span>
-                </div>
-                <div class="col-sm-2">
-                    <span style="font-size: x-large">
-                        <i class="fa fa-linkedin-square footer-icon-brand"></i>
-                    </span>
-                </div>
-                <div class="col-sm-2">
-                    <span style="font-size: x-large">
-                        <i class="fa fa-twitter-square footer-icon-brand"></i>
-                    </span>
-                </div>
+            <div class="col-sm-2" style="margin-right: 30px;">
+                <img src="/MainProject/image/CashOnSpot.png" class="img-footer" id="img-cash" alt="Cash">
+                <p class="txt-footer">پرداخت در محل</p>
             </div>
-
-            <div class="col-sm-6 footer-link-column3">
-                <i class="footer-header-txt">
-                    <i class="fa fa-info footer-icon"></i>با عطرشاپ
-                </i>
-                <br><br>
-                <a href="#" class="footer-link-columns"><i class="fa fa-angle-left footer-icon"></i>تماس با ما</a>
-                <a href="#" class="footer-link-columns" style="margin-bottom: 20px"><i
-                            class="fa fa-angle-left footer-icon"></i>درباره ما</a>
+            <div class="col-sm-2" style="margin-right: 40px;">
+                <img src="/MainProject/image/Express.png" class="img-footer" id="img-express" alt="Express">
+                <p class="txt-footer">تحویل اکسپرس</p>
+            </div>
+            <div class="col-sm-2" style="margin-right: 40px;">
+                <img src="/MainProject/image/Return.png" class="img-footer" id="img-return" alt="Return">
+                <p class="txt-footer">7 روز زمان بازگشت</p>
+            </div>
+            <div class="col-sm-2" style="margin-right: 40px;">
+                <img src="/MainProject/image/Support.png" class="img-footer" id="img-support" alt="Support">
+                <p class="txt-footer">پشتیبانی 24 ساعته</p>
+            </div>
+            <div class="col-sm-2" style="margin-right: 40px;">
+                <img src="/MainProject/image/Warranty.png" class="img-footer" id="img-warranty" alt="Warranty">
+                <p class="txt-footer">ضمانت اصل بودن کالا</p>
             </div>
         </div>
     </div>
+    <hr class="horizontal-line4">
+
+    <div class="footer-link">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5" style="margin-right: 190px;">
+                    <i class="fa fa-info"><span style="font-size: large;margin-right: 7px;">با عطرشاپ</i></span>
+                    <a href="#" class="footer-link-columns" style="margin-top: 20px;"><i
+                                class="fa fa-angle-left footer-icon"></i>تماس با ما</a>
+                    <a href="#" class="footer-link-columns" style="margin-bottom: 20px"><i
+                                class="fa fa-angle-left footer-icon"></i>درباره ما</a>
+                </div>
+
+                <div class="col-sm-5">
+                    <p class="footer-header-net">در شبکه های اجتماعی ما را دنبال کنید</p>
+                    <br><br>
+                    <div style="margin-right: 68px;">
+                        <span class="col-sm-2" style="font-size: x-large;">
+                            <i class="fa fa-youtube-square footer-icon-brand"></i>
+                        </span>
+
+                        <span class="col-sm-2" style="font-size: x-large;">
+                            <i class="fa fa-instagram footer-icon-brand"></i>
+                        </span>
+
+                        <span class="col-sm-2" style="font-size: x-large;">
+                            <i class="fa fa-linkedin-square footer-icon-brand"></i>
+                        </span>
+
+                        <span class="col-sm-2" style="font-size: x-large;">
+                            <i class="fa fa-twitter-square footer-icon-brand"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
-</body>
-</html>
--- theme/defualt.php
+++ theme/defualt.php
@@ -3,8 +3,8 @@
     <script src="<?= baseUrl() ?>js/jquery-2.2.4.js"></script>
     <script src="<?= baseUrl() ?>js/jquery-2.2.4.min.js"></script>
     <link rel="stylesheet" href="<?= baseUrl() ?>css/style.css">
-    <link rel="stylesheet" href="<?= baseUrl() ?>css/bootstrap-rtl.css">
     <link rel="stylesheet" href="<?= baseUrl() ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?= baseUrl() ?>css/bootstrap-rtl.css">
     <link rel="stylesheet" href="<?= baseUrl() ?>css/Font-Awesome-fa-4/css/font-awesome.css">
     <title>به عطرشاپ خوش آمدید</title>
 </head>
