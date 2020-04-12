85241441412389746951d42f4f4c0f79bd0fb094
pooria alimardani <p.javawork@gmail.com>
2020-04-12 14:12:18 +0430

    - Changes on header.php page

--- mvc/view/page/header.php
+++ mvc/view/page/header.php
@@ -1,19 +1,20 @@
 <div class="header">
     <br>
+
     <!-- Register & Login -->
     <?php
     /** @var TYPE_NAME $isGuest */
     $isGuest = !isset($_SESSION['userEmail']);
     if ($isGuest) { ?>
         <div style="margin-right: 40px">
-            <a href="<?= baseUrl() ?>user/login">
+            <a href="<?= baseUrl() ?>user/login" style="text-decoration: none;">
                 <i class="txt-header-register">
                     <i class="fa fa-sign-in" style="margin-left:10px;"></i>ورود</i>
             </a>
 
             <i class="txt-header-register2">|</i>
 
-            <a href="<?= baseUrl() ?>user/register" style="margin-left: 50px">
+            <a href="<?= baseUrl() ?>user/register" style="margin-left: 50px;text-decoration: none;">
                 <i class="txt-header-register">
                     <i class="fa fa-user-plus" style="margin-left:10px;"></i>ثبت نام</i>
             </a>
@@ -24,7 +25,7 @@
             </span>
             <span class="btn-cart vertical-menu">
                 <div id="cartPreviewHolder"
-                     style="width:220px;display: none;background-color: white;position: relative;box-shadow: 0 0 5px #a2a2a2;border-radius: 5px;"></div>
+                     style="width:250px;display: none;background-color: white;position: relative;box-shadow: 0 0 5px #a2a2a2;border-radius: 5px;"></div>
             </span>
         </div>
     <?php } ?>
@@ -32,7 +33,7 @@
     <!-- Buy -->
     <?php if (!$isGuest) { ?>
         <span class="exit-btn">
-            <a href="/MainProject/user/logout" style="color: #555;">
+            <a href="/MainProject/user/logout" style="color: #555;font-size: 17px;">
                 <i class="fa fa-sign-out signout-btn"></i>خروج
             </a>
         </span>
@@ -48,17 +49,29 @@
     <?php } ?>
 
     <!-- Logo -->
-    <img src="/MainProject/image/Logo3.jpg" id="imgLogo" alt="لگو صفحه">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-1">
+                <img src="/MainProject/image/Logo3.jpg" id="imgLogo" alt="لگو صفحه">
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- Navigation Bar -->
 <div class="navbar">
     <a href="<?= baseUrl() ?>page/home" class="menu-item">
-        <i class="fa fa-home menu-icon"></i>صفحه اصلی
+        <i class="fa fa-home menu-icon"></i>
+        <span style="font-size: 18px;">
+            صفحه اصلی
+        </span>
     </a>
 
     <a href="<?= baseUrl() ?>productsman/productsman" class="menu-item">
-        <i class="fa fa-pinterest-p menu-icon"></i>محصولات
+        <i class="fa fa-pinterest-p menu-icon"></i>
+        <span style="font-size: 18px;">
+            محصولات
+        </span>
     </a>
 
     <?php if ($isGuest) {
@@ -66,18 +79,21 @@
     } elseif (!$isGuest && $_SESSION['userAccess'] == 'user') { ?>
         <a class="menu-item" href="/MainProject/productsman/myorders">
             <img src="/MainProject/image/EmptyProfile.png" class="profile-img" alt="پروفایل">
-            <span style="font-size: medium;"><?= $_SESSION['userEmail'] ?></span>
-            <span style="margin-right: 7px;">کاربر</span>
+            <span style="font-size: 18px;"><?= $_SESSION['userEmail'] ?></span>
+            <span style="margin-right: 7px;font-size: 18px;">کاربر</span>
         </a>
     <?php } elseif (!$isGuest && $_SESSION['userAccess'] == 'user,admin') { ?>
-        <a class="menu-item" href="/MainProject/productsman/myorders">
-            <img src="/MainProject/image/EmptyProfile.png" class="profile-img" alt="پروفایل">
-            <span style="font-size: medium;"><?= $_SESSION['userEmail'] ?></span>
-            <span style="margin-right: 7px;">مدیرسایت</span>
+        <a href="<?= baseUrl() ?>page/manager" class="menu-item">
+            <i class="fa fa-user-circle menu-icon"></i>
+            <span style="font-size: 18px;">
+                فرم مدیریت سایت
+            </span>
         </a>
 
-        <a href="<?= baseUrl() ?>page/manager" class="menu-item">
-            <i class="fa fa-pinterest-p menu-icon"></i> فرم مدیریت سایت
+        <a class="menu-item" href="/MainProject/productsman/myorders">
+            <img src="/MainProject/image/EmptyProfile.png" class="profile-img" alt="پروفایل">
+            <span style="font-size: 18px;"><?= $_SESSION['userEmail'] ?></span>
+            <span style="margin-right: 7px;font-size: 18px;">مدیرسایت</span>
         </a>
     <?php }
     a: ?>
